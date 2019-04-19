b8ec67ada08f92250be79d70d8b2e7a143274ea3
rcampello <rafael.campello@hotmail.com>
2019-04-19 15:15:24 -0300

    Scripts.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 IT++ no GNU Radio (Em construção)
 =============================
 
-Branch composto de blocos testes/experimentos. Apenas para ver algumas ideias de implementação. Preferencialmente não instalar (com cmake../ e sudo make install).
+Branch composto de blocos testes/experimentos. Apenas para ver algumas ideias de implementação. 
+
+<!-- Preferencialmente não instalar (com cmake../ e sudo make install). -->
+
+
+
+
+Transferência de Imagem SIMULADA - Sem Tratamento de Recepção Extra.      |  
+:-------------------------:|
+![](examples/Scripts&#32;Python/Scripts/BPSK/script_images.png) |  
---
+++ examples/Scripts Python/Scripts/BPSK/import_me.py
@@ -0,0 +1,69 @@
+import numpy as np
+import cv2
+import os
+
+def readSave(readfileName, savefileName = "output.bmp", save = False, delete = False):
+    with open(readfileName, "rb") as f:
+        b = f.read()
+    rcv = np.frombuffer(b, dtype = np.uint8)
+    r = saveRcvImage(savefileName, rcv, save = save)
+    if delete == True:
+        os.remove(readfileName)
+    return r
+
+def rcv_ready(rcv, shape):
+    rcv = trata_rcv(rcv)
+    r = rcv[122 : 122 + shape[0] * shape[1] * 3].reshape(shape[0], shape[1], 3)
+    return cv2.cvtColor(r, cv2.COLOR_BGR2RGB)
+
+def trata_rcv(rcv):
+    tupla = findBitShift_and_Begin(rcv)
+    r = np.roll(np.packbits(np.roll(np.unpackbits(rcv), tupla[0])), -tupla[1])
+    return r
+
+
+def saveRcvImage(fileName, rcv, save = False):
+    tupla = findBitShift_and_Begin(rcv)
+    gravar = np.roll(np.packbits(np.roll(np.unpackbits(rcv), tupla[0])), -tupla[1])
+    if save:
+        with open(fileName, "wb") as f:
+            f.write(gravar)
+    return gravar
+
+
+def findBitShift_and_Begin(rcv_bitmap):
+    for i in range(8):
+        aux = search_sequence_numpy(np.packbits(np.roll(np.unpackbits(rcv_bitmap), i)), np.array([66, 77]))
+        if len(aux) != 0:
+            return (i, aux[0])
+
+def search_sequence_numpy(arr,seq):
+    """ Find sequence in an array using NumPy only.
+
+    Parameters
+    ----------    
+    arr    : input 1D array
+    seq    : input 1D array
+
+    Output
+    ------    
+    Output : 1D Array of indices in the input array that satisfy the 
+    matching of input sequence in the input array.
+    In case of no match, an empty list is returned.
+    """
+
+    # Store sizes of input array and sequence
+    Na, Nseq = arr.size, seq.size
+
+    # Range of sequence
+    r_seq = np.arange(Nseq)
+
+    # Create a 2D array of sliding indices across the entire length of input array.
+    # Match up with the input sequence & get the matching starting indices.
+    M = (arr[np.arange(Na-Nseq+1)[:,None] + r_seq] == seq).all(1)
+
+    # Get the range of those indices as final output
+    if M.any() >0:
+        return np.where(np.convolve(M,np.ones((Nseq),dtype=int))>0)[0]
+    else:
+        return []         # No match found
